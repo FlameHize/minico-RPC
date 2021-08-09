9f0e060bc7d9c3dcc43e87b275038ca9a291938e
zhuguangxu.zgx <zhuguangxu.zgx@antgroup.com>
2021-08-09 21:02:25 +0800

    rpc_server commit

--- include/minico_api.h
+++ include/minico_api.h
@@ -4,25 +4,14 @@
 
 namespace minico 
 {
-	////////////////////协程调用相关接口///////////////////
 
-	//并发运行一个协程，协程将运行func函数
-	//可以指定协程栈大小stackSize，默认为2k
-	//tid：协程要运行在哪个线程上
-	//-1： 使用netco调度器选择协程运行在哪个线程
-	//other：线程号
-	//netco会根据处理器个数创建等数量的线程用于运行协程，
-	//若有4个处理器，则tid只有0 ~ 3是有效的 
 	void co_go(std::function<void()>& func, size_t stackSize = parameter::coroutineStackSize, int tid = -1);
 	void co_go(std::function<void()>&& func, size_t stackSize = parameter::coroutineStackSize, int tid = -1);
 
-	//当前协程等待t毫秒后再继续执行
 	void co_sleep(Time t);
 
-	//等待调度器的结束
 	void sche_join();
 
-	//////////////网络相关系统调用使用Socket.h头中的封装//////////
 }
 
 
--- include/rpc_server.h
+++ include/rpc_server.h
@@ -1,1 +1,70 @@
+#pragma once
 
+#include "../include/tcp_server.h"
+#include "../include/json.h"
+
+/** Service是一个接口,表示一种服务,一个RpcServer可以包含多个Service*/
+class Service
+{
+  public:
+    Service() = default;
+    virtual ~Service() = default;
+
+    virtual const char* name() const  = 0;
+
+    /** 实际进行服务数据处理的功能函数*/
+    virtual void process(const TinyJson& request,TinyJson& result) = 0;
+};
+
+/**
+ * @brief 基于协程的rpc服务器
+*/
+class RpcServer
+{
+  public:
+    DISALLOW_COPY_MOVE_AND_ASSIGN(RpcServer);
+
+    RpcServer();
+    ~RpcServer();
+
+    /** 
+     * 向服务器中添加一种服务,一种服务可以有多个接口方法
+     * s必须是new创建的，方便统一管理
+     */
+    void add_service(Service* s);
+
+    /**
+     * 开启rpc服务器的运行
+    */
+    void start(const char* ip,int port);
+
+  private:
+    /** 先保存一个空指针 保存一个tcp的服务器也可以*/
+    void* _p;
+};
+
+/**
+ * rpc客户端，其功能必须要在一个协程中运行
+*/
+class RpcClient
+{
+  public:
+    Client(const char* ip,int port);
+    Client(const Client& c);
+    ~Client();
+
+    void operator=(const Client& c) = delete;
+
+    /**
+     * @brief 进行一次rpc请求，从result形参中直接得到结果
+    */
+    void call(const TinyJson& result,TinyJson& result);
+
+    /** 客户端心跳检测*/
+    void ping();
+
+    void close();
+  
+  private:
+    void* _p;
+};
